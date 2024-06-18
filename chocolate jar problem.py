e0d2652314e2434d50f1d4f1b8e902ae3ada749c
bshirishareddy <bshirishareddyreddy@gmail.com>
2024-06-18 12:13:38 +0530

    Add files via upload

---
+++ chocolate jar problem.py
@@ -0,0 +1,21 @@
+jars=list(map(int,input().split()))
+n=int(input())
+a=0
+for i in jars:
+    a=a+(i//3)
+    if i%3!=0:
+        a=a+1
+    else:
+        a=a+0
+        print(a)
+
+
+       
+        
+            
+            
+            
+            
+            
+        
+
