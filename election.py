ba3a29a379af8569b05427ebd329fa0732e96f56
bshirishareddy <bshirishareddyreddy@gmail.com>
2024-06-18 12:15:05 +0530

    Add files via upload

---
+++ election.py
@@ -0,0 +1,18 @@
+class election:
+    def election_party():
+       print("party 1,4,5")
+       n=int(input("enter the party"))
+       r=0
+       if n==1:
+           r==1
+       if n==4:
+           r==4
+       if n==5:
+           r==5
+           print(r)    
+       else:
+             print("invalid party")
+a=election()
+a.election_party()            
+       
+
